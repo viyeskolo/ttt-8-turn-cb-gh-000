66543e2d682568e18ebf84958e386384d90826af
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-26 02:07:16 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+  display_board
+valid_move?
+move
+
+require_relative '../lib/turn'
+ 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ 
+puts "Welcome to Tic Tac Toe!"
+display_board(board)
